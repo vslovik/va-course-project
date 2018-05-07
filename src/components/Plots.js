2a8e5f1fa0eea2ba2d65207c31ba56906890ddc5
Valeriya Slovikovskaya <valeriya.slovikovskaya@gruppometa.it>
2018-05-07 09:02:32 +0200

    polar scatter plot

--- src/components/Plots.js
+++ src/components/Plots.js
@@ -52,72 +52,79 @@ export default class Plots extends Component {
                 }
 
                 console.log('winData', windData);
+                let data = windData;
+                let width = 800,
+                    height = 600,
+                    radius = Math.min(width, height) / 2 - 30;
+
+                let r = scaleLinear()
+                    .domain([0, .5])
+                    .range([0, radius]);
+
+                let line = radialLine()
+                    .radius(function(d) { return r(d[1]); })
+                    .angle(function(d) { return -d[0] + Math.PI / 2; });
+
+                let svg = select("td.plot1")
+                    .append("svg")
+                    .attr("width", width)
+                    .attr("height", height)
+                    .append("g")
+                    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+                let gr = svg.append("g")
+                    .attr("class", "r axis")
+                    .selectAll("g")
+                    .data(r.ticks(5).slice(1))
+                    .enter().append("g");
+
+                gr.append("circle")
+                    .attr("r", r);
+
+                gr.append("text")
+                    .attr("y", function(d) { return -r(d) - 4; })
+                    .attr("transform", "rotate(15)")
+                    .style("text-anchor", "middle")
+                    .text(function(d) { return d; });
+
+                let ga = svg.append("g")
+                    .attr("class", "a axis")
+                    .selectAll("g")
+                    .data(range(0, 360, 30))
+                    .enter().append("g")
+                    .attr("transform", function(d) { return "rotate(" + -d + ")"; });
+
+                ga.append("line")
+                    .attr("x2", radius);
+
+                ga.append("text")
+                    .attr("x", radius + 6)
+                    .attr("dy", ".35em")
+                    .style("text-anchor", function(d) { return d < 270 && d > 90 ? "end" : null; })
+                    .attr("transform", function(d) { return d < 270 && d > 90 ? "rotate(180 " + (radius + 6) + ",0)" : null; })
+                    .text(function(d) { return d + "°"; });
+
+                // svg.append("path")
+                //     .datum(data)
+                //     .attr("class", "line")
+                //     .attr("d", line);
+
+                svg.selectAll("point")
+                    .data(data)
+                    .enter()
+                    .append("circle")
+                    .attr("class", "point")
+                    .attr("transform", function(d) {
+                        let coors = line([d]).slice(1).slice(0, -1);
+                        return "translate(" + coors + ")"
+                    })
+                    .attr("r", 2)
+                    .attr("fill",function(d,i){
+                        return 'black'//color(i);
+                    })
+                ;
 
             });
-
-        let data = windData
-
-        // let data = range(0, 2 * Math.PI, .01).map(function(t) {
-        //     return [t, Math.sin(2 * t) * Math.cos(2 * t)];
-        // })
-
-        console.log('data', data)
-
-        let width = 800,
-            height = 600,
-            radius = Math.min(width, height) / 2 - 30;
-
-        let r = scaleLinear()
-            .domain([0, .5])
-            .range([0, radius]);
-
-        let line = radialLine()
-            .radius(function(d) { return r(d[1]); })
-            .angle(function(d) { return -d[0] + Math.PI / 2; });
-
-        let svg = select("td.plot1")
-            .append("svg")
-            .attr("width", width)
-            .attr("height", height)
-            .append("g")
-            .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
-
-        let gr = svg.append("g")
-            .attr("class", "r axis")
-            .selectAll("g")
-            .data(r.ticks(5).slice(1))
-            .enter().append("g");
-
-        gr.append("circle")
-            .attr("r", r);
-
-        gr.append("text")
-            .attr("y", function(d) { return -r(d) - 4; })
-            .attr("transform", "rotate(15)")
-            .style("text-anchor", "middle")
-            .text(function(d) { return d; });
-
-        let ga = svg.append("g")
-            .attr("class", "a axis")
-            .selectAll("g")
-            .data(range(0, 360, 30))
-            .enter().append("g")
-            .attr("transform", function(d) { return "rotate(" + -d + ")"; });
-
-        ga.append("line")
-            .attr("x2", radius);
-
-        ga.append("text")
-            .attr("x", radius + 6)
-            .attr("dy", ".35em")
-            .style("text-anchor", function(d) { return d < 270 && d > 90 ? "end" : null; })
-            .attr("transform", function(d) { return d < 270 && d > 90 ? "rotate(180 " + (radius + 6) + ",0)" : null; })
-            .text(function(d) { return d + "°"; });
-
-        svg.append("path")
-            .datum(data)
-            .attr("class", "line")
-            .attr("d", line);
     }
 
     componentDidMount_(state) {
