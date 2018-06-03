28569058f53d1e0c61fa9ff42312af84e54209f6
Valeriya <vslovik@gmail.com>
2018-06-03 18:33:31 +0200

    circular chart: wind chart wip

--- src/wind-chart/index.js
+++ src/wind-chart/index.js
@@ -5,17 +5,13 @@ export default function windChart(response) {
 
     let rows = csvParseRows(response.responseText).slice(1);
 
-    console.log(rows);
-
     // 0.1 6.8
-    let i, max = 0, min = Number.POSITIVE_INFINITY, measure, segments = 36, aSegment, angleStep = 10.0, mSegment, mStep = 1.0;
+    let i, max = 0, min = Number.POSITIVE_INFINITY, measure, aSegments = 36, aSegment, angleStep = 10.0, mSegment, mSegments = 7, mStep = 1.0;
 
     let ddd = {};
-    [...Array(36*7).keys()].forEach(function(i) {
+    [...Array(aSegments*mSegments).keys()].forEach(function(i) {
         ddd[i] = 0;
     });
-
-    console.log(ddd)
 
     rows.forEach(function(row) {
         let dt, angle, value;
@@ -29,9 +25,7 @@ export default function windChart(response) {
         }
     });
 
-    console.log('min', min, 'max', max)
-
-
+    // ToDo Remove magic constants
     let mMax = 0, parseDateTime, t;
     rows.forEach(function (row) {
         let dt, angle, value;
@@ -48,60 +42,45 @@ export default function windChart(response) {
 
             t = parseDateTime(dt);
 
-            // if (t.getMonth() === 3 && t.getDate() === 2) {
-
-
-
-                console.log(dt, angle, value);
+            if (t.getMonth() === 11) { //&& t.getDate() === 2
 
                 angle = parseFloat(angle.replace(',', '.'));
                 measure = parseFloat(value.replace(',', '.'));
 
-
                 aSegment = Math.floor(angle / angleStep);
                 if (aSegment === 36) aSegment = 35;
-
-                console.log('aSegment', aSegment);
 
                 mSegment = Math.floor(measure / mStep);
                 if (mSegment === 7) mSegment = 6;
 
-                console.log('mSegment', mSegment);
-
                 // Get i - segment index: i = <angle segment index> * <number of measure segments> + <measure segment index>
-                i = mSegment * segments + aSegment;
+                i = mSegment * aSegments + aSegment;
                 if (i === 252) i = 251;
 
                 ddd[i] += 1;
                 if (ddd[i] > mMax) {
                     mMax = ddd[i];
                 }
-
-
-
-            // }
+            }
         }
     });
 
     for(let i = 0; i < 252; i++) ddd[i] = ddd[i] / mMax;
 
-    console.log(min, max, segments, ddd);
     let data = Object.values(ddd);
 
     /* Random data */
     // let data = [];
     // for(let i = 0; i < 252; i++) data[i] = Math.random();
 
-    console.log(data);
+    let radialLabels  =  [...Array(7).keys()];
+    let segmentLabels = [...Array(36).keys()].map(function(x) { return x * 10 });
 
     let chart = circularHeatChart()
         .innerRadius(20)
         .range(["white", "steelblue"])
-        .radialLabels(["1.", "2.", "3.", "4.", "5.", "6.", "7.", "8."])
-        .segmentLabels(["0", "10", "20", "30", "40", "50", "60", "70", "80", "90",
-            "100", "110", "120", "130", "140", "150", "160", "170", "180",
-            "190", "200", "210", "220", "230","240", "250", "260", "270",
-        "280", "290", "300", "310", "320", "330", "340", "350", "360"]);
+        .radialLabels(radialLabels)
+        .segmentLabels(segmentLabels);
 
     select('td.plot1')
         .selectAll('svg')
