284674a265cd976e001d71f04ca593690596b305
Valeriya <vslovik@gmail.com>
2018-06-06 20:31:34 +0200

    scatter chart: cleanup

--- src/scatter-chart/index.js
+++ src/scatter-chart/index.js
@@ -2,51 +2,56 @@ import {csvParseRows,
     max, min, scaleLinear, scaleSqrt, scaleTime, select,
     timeFormat, timeParse, axisLeft, axisBottom} from "d3";
 
-const APP = 1;
-const CHL = 2;
-const MET = 3;
-const AGO = 4;
+class Data {
 
-const getDataset = (rows, structure = 'SenMonChe') => {
+    constructor()
+    {
+        const [APP, CHL, MET, AGO] = Data.getChemicalsEncoding();
 
-    const chemicals = {
-        "Appluimonia": APP,
-        "Chlorodinine": CHL,
-        "Methylosmolene": MET,
-        "AGOC-3A": AGO
-    };
+        this.chemicals = {
+            "Appluimonia": APP,
+            "Chlorodinine": CHL,
+            "Methylosmolene": MET,
+            "AGOC-3A": AGO
+        };
+    }
 
-    let dataset = {};
+    static getChemicalsEncoding() {
+        return [1, 2, 3, 4];
+    }
 
-    let sensors = Array.from(Array(9).keys());
+    static parseMeasureDate(dt) {
+        let parser;
 
-    // Chemical,Monitor,DateTime,Reading
-    let che, sen, val, t, rt, mon;
-    for (let i = 1; i < rows.length; i++) {
-
-        sen = parseInt(rows[i][1]);
-        che = chemicals[rows[i][0]];
-        val = parseFloat(rows[i][3].replace(',', '.'));
-
-        let parseDateTime;
-
-        if(rows[i][2].length === '2016/01/01'.length){
-            parseDateTime = timeParse("%Y/%m/%d");
-        } else if(rows[i][2].length === "2016/04/01 08:00:00".length) {
-            parseDateTime = timeParse("%Y/%m/%d %H:%M:%S");
-        } else {
-            throw 'Value: "' + rows[i][0] + '" is not a date'
+        if (dt.length === '2016/01/01'.length) {
+            parser = timeParse("%Y/%m/%d");
+            dt = parser(dt)
         }
 
-        t = parseDateTime(rows[i][2]);
-
-        if(null === t) {
-            console.log('DateTime parse error' + rows[i][2])
+        if (dt.length === "2016/04/01 08:00:00".length) {
+            parser = timeParse("%Y/%m/%d %H:%M:%S");
+            dt = parser(dt)
         }
 
-        mon = t.getMonth();
+        if (null === dt)
+            console.log('Value: "' + dt + '" is not a date');
 
-        if(structure === 'SenMon') {
+        return dt;
+    }
+
+    collectStructuredData(row, structure, dataset) {
+
+        let [che, sen, dt, val] = row;
+
+        che = this.chemicals[che];
+        sen = parseInt(sen);
+        val = parseFloat(val.replace(',', '.'));
+
+        let t = Data.parseMeasureDate(dt);
+
+        let mon = t.getMonth();
+
+        if (structure === 'SenMon') {
             if (dataset[sen]) {
                 if (dataset[sen][mon]) {
                     dataset[sen][mon].push({
@@ -81,85 +86,63 @@ const getDataset = (rows, structure = 'SenMonChe') => {
         }
     }
 
-    return dataset;
-};
+    getData(rows, structure = 'SenMonChe') {
+
+        let data = {};
+
+        for (let i = 1; i < rows.length; i++) {
+            this.collectStructuredData(rows[i], structure, data)
+        }
+
+        return data;
+    };
+}
 
 class Chart {
     constructor(selector, data){
 
-        this.data = data;
-
+        this.data    = data;
         this.w       = 800;
         this.h       = 600;
         this.padding = 40;
 
-        this.createScales();
+        const [APP, CHL, MET, AGO] = Data.getChemicalsEncoding();
 
-        let yAxis = axisLeft()
-            .scale(this.yScale)
-            .ticks(10);
+        const colorMap = {};
 
-        let formatTime = timeFormat("%e");
+        colorMap[APP] = 'red';
+        colorMap[CHL] = 'orange';
+        colorMap[MET] = 'blue';
+        colorMap[AGO] = 'green';
 
-        let xAxis = axisBottom()
-                .scale(this.xScale)
-                .tickFormat(formatTime)
-            // .ticks(10)
-        ;
+        this.colorMap =  colorMap;
 
-        let svg = select(selector)
+        this.svg = select(selector)
             .append("svg")
             .attr("width", this.w)
             .attr("height", this.h);
 
-        let chart = this;
-
-        svg.selectAll("circle")
-            .data(this.data)
-            .enter()
-            .append("circle")
-            .attr("fill", function(d) {
-
-                let color;
-                let colorMap = {};
-                colorMap[APP] = 'red';
-                colorMap[CHL] = 'orange';
-                colorMap[MET] = 'blue';
-                colorMap[AGO] = 'green';
-
-                return colorMap[d.che];
-            })
-            .attr("cx", function(d, i) {
-                return chart.xScale(d.t);
-            })
-            .attr("cy", function(d) {
-                return chart.yScale(d.val);
-            })
-            .attr("r", function(d) {
-                return chart.aScale(d.val);
-            });
-
-        svg.append("g")
-            .attr("class", "axis")
-            .attr("transform", "translate(0," + (this.h - this.padding) + ")")
-            .call(xAxis);
-
-        svg.append("g")
-            .attr("class", "axis")
-            .attr("transform", "translate(" + this.padding + ",0)")
-            .call(yAxis);
+        this.createScales()
+            .drawPoints()
+            .addAxes();
     }
 
     createScales() {
         let chart = this;
 
         this.xScale = scaleTime()
-            .domain([min(this.data, function(d) { return d.t; }), max(chart.data, function(d) { return d.t; })])
+            .domain([
+                min(this.data, function(d) { return d.t; }),
+                max(this.data, function(d) { return d.t; })
+            ])
             .range([this.padding, this.w - this.padding])
             .nice();
 
         this.yScale = scaleLinear()
-            .domain([min(this.data, function(d) { return d.val; }), max(chart.data, function(d) { return d.val; })])
+            .domain([
+                min(this.data, function(d) { return d.val; }),
+                max(chart.data, function(d) { return d.val; })
+            ])
             .rangeRound([this.h - this.padding, this.padding])
             .nice();
 
@@ -170,17 +153,61 @@ class Chart {
         return this;
     }
 
-}
+    drawPoints() {
+        let chart = this;
 
+        this.svg.selectAll("circle")
+            .data(this.data)
+            .enter()
+            .append("circle")
+            .attr("fill", function (d) {
+                return chart.colorMap[d.che];
+            })
+            .attr("cx", function (d, i) {
+                return chart.xScale(d.t);
+            })
+            .attr("cy", function (d) {
+                return chart.yScale(d.val);
+            })
+            .attr("r", function (d) {
+                return chart.aScale(d.val);
+            });
+
+        return this;
+    }
+
+    addAxes() {
+        let yAxis = axisLeft()
+            .scale(this.yScale)
+            .ticks(10);
+
+        let xAxis = axisBottom()
+            .scale(this.xScale)
+            .tickFormat(timeFormat("%e"));
+        // .ticks(10)
+
+        this.svg.append("g")
+            .attr("class", "axis")
+            .attr("transform", "translate(0," + (this.h - this.padding) + ")")
+            .call(xAxis);
+
+        this.svg.append("g")
+            .attr("class", "axis")
+            .attr("transform", "translate(" + this.padding + ",0)")
+            .call(yAxis);
+
+        return this;
+    }
+}
 
 export default function scatterChart(response) {
 
     let rows = csvParseRows(response.responseText);
 
-    // let dataset = getDataset(rows, 'SenMonChe');
+    // let dataset = getData(rows, 'SenMonChe');
     // let dd = dataset[1][3][1]; // 7, 11
 
-    let dataset = getDataset(rows, 'SenMon');
+    let dataset = (new Data()).getData(rows, 'SenMon');
     let data = dataset[1][3]; // 7, 11
 
     new Chart('td.plot1', data);
