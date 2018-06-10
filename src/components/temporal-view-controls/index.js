4909ef6b09bb2734c4e236d331d9d84c411911a5
Valeriya <vslovik@gmail.com>
2018-06-11 00:17:40 +0200

    new layout, buttons sass

--- src/components/temporal-view-controls/index.js
+++ src/components/temporal-view-controls/index.js
@@ -5,40 +5,20 @@ import MonthFilter from '../filter/month'
 export default class TemporalViewControls extends Component {
     render = () => (
         <div className="reponav-controls container">
-
-            {/*<h1> Sensor 4 </h1>*/}
-
-            {/*<span className="counter">?</span>*/}
-
-            {/*<span className="controls">*/}
-            {/*<a href="" className="button">Linear</a>*/}
-            {/*<a href="" className="button">Log</a>*/}
-            {/*</span>*/}
-
-            {/*<span className="controls">*/}
-            {/*<a href="" className="button">Day</a>*/}
-            {/*<a href="" className="button">Hour</a>*/}
-            {/*</span>*/}
-
-            {/*<MonthFilter/>*/}
-
             <span className="controls">
-                <a href="" className="agog-button_">Sensor 4</a>
-                <a href="" className="question-button">?</a>
+                <a href="" className="button-sensor">SENSOR 4</a>
+                <a href="" className="button-question">?</a>
             </span>
-
             <span className="controls">
-                <a href="" className="toggle-button">Linear</a>
-                <a href="" className="appl-button">Log</a>
+                <a href="" className="toggle-button-left">Linear</a>
+                <a href="" className="toggle-button-right">Log</a>
             </span>
-
             <span className="controls">
-                <a href="" className="toggle-button">Day</a>
-                <a href="" className="appl-button">Hour</a>
+                <a href="" className="toggle-button-left">Day</a>
+                <a href="" className="toggle-button-right">Hour</a>
             </span>
 
             <ChemicalFilter/>
-
         </div>
     )
 }
