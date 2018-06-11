0392ae46f87476fd7f8a739cacb0005e34d9085c
Valeriya Slovikovskaya <valeriya.slovikovskaya@gruppometa.it>
2018-06-11 09:33:58 +0200

    state draft

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,4 +1,8 @@
 import React, {Component} from 'react'
+
+import PropTypes from "prop-types";
+import {connect} from 'react-redux'
+
 import Tabs from './../tabs'
 import VectorialViewControls from "../vectorial-view-controls";
 import TemporalViewControls from "../temporal-view-controls";
@@ -7,15 +11,21 @@ import {VECTORIAL, TEMPORAL} from '../../constants'
 import {AGOG, APPL, CHLO, METH, ALL} from '../../constants';
 import {APR, AUG, DEC}  from '../../constants';
 
-export default class Footer extends Component {
+export default class Header extends Component {
 
-    state = {
-        data: [], //?
-        view: TEMPORAL,
-        chemical: ALL,
-        month: ALL,
-        sensor: ALL
-    };
+    constructor() {
+        super();
+
+        this.state = {
+            data: [], //?
+            view: TEMPORAL,
+            chemical: ALL,
+            month: ALL,
+            sensor: ALL
+        };
+
+        // this.nextStep = this.nextStep.bind(this);
+    }
 
     render = () => (
         <div>
@@ -24,3 +34,30 @@ export default class Footer extends Component {
         </div>
     )
 }
+
+// const mapStateToProps = state => {
+//     return {
+//         profile: state.wizard.profile,
+//         userGroupId: state.wizard.userGroupId,
+//         step: state.wizard.step,
+//         schoolType: state.schoolType.schoolType,
+//         districtName: state.schoolProps.districtName
+//     };
+// };
+//
+// const mapDispatchToProps = dispatch => {
+//     return {
+//         selectProfile: profile => dispatch(selectProfile(profile)),
+//         nextStep: () => dispatch(nextStep()),
+//         selectSchoolName: (schoolName) => dispatch(selectSchoolName(schoolName)),
+//         selectDistrictName: (districtName) => dispatch(selectDistrictName(districtName))
+//     };
+// };
+
+// const Wizard = connect(mapStateToProps, mapDispatchToProps)(ConnectedWizard);
+//
+// ConnectedWizard.propTypes = {
+//     nextStep: PropTypes.func.isRequired
+// };
+//
+// export default Wizard;
